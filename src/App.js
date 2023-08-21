bce867d9cea99dc241bda9d0aee47b141e17ec67
dawid <dawtom97@gmail.com>
2023-08-21 18:05:22 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-// 17c82726588bd065f78c9991dde2a619
 
-function App() {
+
+const Header = () => {
+
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    getData();
+  }, [query]);
+
+
+  console.log(data)
+
+  const getData = async () => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1`);
+      // if (!response.ok) {
+        console.log(response);
+        const result = await response.json();
+        setData(result.results);
+
+    } catch (error) {
+      setError("Nie udało się pobrać danych");
+      // } finally {
+      //   setLoading(false);
+    }
+
+  };
+
+  console.log(data);
+  console.log(query);
   return (
     <div>
-       <h2>Hello World</h2>
+
+      <div >
+
+      </div>
+      <input placeholder="szukaj" onChange={(e) => setQuery(e.target.value)} value={query} /> <p ></p>
     </div>
   );
-}
+};
 
-export default App;
+export default Header;
